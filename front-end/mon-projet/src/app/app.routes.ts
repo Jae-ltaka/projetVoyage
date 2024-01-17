d5bcb624e251058255394f6448c8bef24674cd7e
Jaeltaka <takasjaels@yahoo.com>
2024-01-17 11:08:25 +0100

    ajout des 3 premier composant

--- front-end/mon-projet/src/app/app.routes.ts
+++ front-end/mon-projet/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { FirstPageComponent } from './first-page/first-page.component';
+import { InscriptionComponent } from './inscription/inscription.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'connexion', component: ConnexionComponent },
+    {path:'aston',component:FirstPageComponent},
+    {path:'inscription',component:InscriptionComponent},
+];
---
+++ front-end/mon-projet/src/app/inscription/inscription.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-inscription',
+  standalone: true,
+  imports: [],
+  templateUrl: './inscription.component.html',
+  styleUrl: './inscription.component.css'
+})
+export class InscriptionComponent {
+
+}
