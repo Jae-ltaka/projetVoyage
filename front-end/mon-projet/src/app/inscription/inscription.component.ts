cf0f33d9eace3452cbacde9d95624dd9a32698e5
Jaeltaka <takasjaels@yahoo.com>
2024-02-02 11:18:30 +0100

    route

--- front-end/mon-projet/src/app/inscription/inscription.component.ts
+++ front-end/mon-projet/src/app/inscription/inscription.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,6 +8,10 @@ import { Router } from '@angular/router';
   templateUrl: './inscription.component.html',
   styleUrl: './inscription.component.css'
 })
-export class InscriptionComponent {
-
+export class InscriptionComponent implements OnInit{
+ 
+  ngOnInit(): void {
+      
+  }
+  
 }
